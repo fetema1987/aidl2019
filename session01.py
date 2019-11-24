ea8c9ca10884a90f9551ccf2a0a4bcec3568767b
Fernando Tebar <fetema1987@gmail.com>
2019-11-24 17:28:18 +0100

    first commit

---
+++ session01.py
@@ -0,0 +1,24 @@
+import random
+class DataDistribution:
+
+  def __init__ (self, b=None, w=None):
+
+    self.w = w or random.random() + 5
+    self.b = b or random.random()
+    # self.b = random.uniform(-5, 5)
+
+  def generate (self, num_iters):
+
+      for step in itertools.count(0, 1):
+          if num_iters is not None and num_iters == step:
+              break
+          x = random.random()
+          y = self.w * x + self.b
+          yield x, y
+
+  def __call__(self, num_iters=None):
+    return self.generate(num_iters=num_iters)
+
+
+test = DataDistribution()
+print(test(5))
